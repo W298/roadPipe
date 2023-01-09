260e2b0c1bd51fb8cabe8e74bbe8871131520779
W298 <lutca1320@gmail.com>
2023-01-09 12:59:48 +0900

    Fade In / Fade Out BGM (Fix #18)

--- Assets/Scripts/MenuBGM.cs
+++ Assets/Scripts/MenuBGM.cs
@@ -2,24 +2,99 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+
+public enum BGMType
+{
+    NONE,
+    STAGE,
+    MENU
+}
 
 public class MenuBGM : MonoBehaviour
 {
-    private AudioSource audioSource;
+    private AudioSource menuAudioSource;
+    private AudioSource stageAudioSource;
+
+    private BGMType prevBGMType = BGMType.NONE;
+    private BGMType currentBGMType = BGMType.NONE;
+
+    private IEnumerator menuAudioFadeOut = null;
+    private IEnumerator stageAudioFadeOut = null;
+
+    private IEnumerator menuAudioFadeIn = null;
+    private IEnumerator stageAudioFadeIn = null;
+
+    public BGMDB stageBGMDB;
+    public AudioClip menuBGM;
 
     private void OnSceneLoad(Scene scene, LoadSceneMode mode)
     {
-        if (scene.name != "MainMenu" && scene.name != "StageSelect" && gameObject != null)
+        if (gameObject == null) return;
+
+        prevBGMType = currentBGMType;
+        currentBGMType = scene.name is "MainMenu" or "StageSelect" ? BGMType.MENU : BGMType.STAGE;
+
+        if (prevBGMType == currentBGMType) return;
+
+        if (currentBGMType == BGMType.MENU)
         {
-            Destroy(gameObject);
+            if (stageAudioSource.isPlaying) FadeOutAudio(BGMType.STAGE);
+            
+            menuAudioSource.clip = menuBGM;
+            FadeInAudio(BGMType.MENU, 0.7f);
+        }
+        else
+        {
+            if (menuAudioSource.isPlaying) FadeOutAudio(BGMType.MENU);
+
+            var levelBGM = stageBGMDB.levels.Find(level => level.name == scene.name[0].ToString());
+            stageAudioSource.clip = levelBGM.defaultBGMClip;
+            FadeInAudio(BGMType.STAGE, 1f);
+        }
+    }
+
+    private void FadeInAudio(BGMType type, float maxVolume)
+    {
+        var source = type == BGMType.MENU ? menuAudioSource : stageAudioSource;
+        var fadeOutCoroutine = type == BGMType.MENU ? menuAudioFadeOut : stageAudioFadeOut;
+
+        if (fadeOutCoroutine != null) StopCoroutine(fadeOutCoroutine);
+        if (type == BGMType.MENU)
+        {
+            menuAudioFadeIn = FadeIn(source, maxVolume);
+            StartCoroutine(menuAudioFadeIn);
+        }
+        else
+        {
+            stageAudioFadeIn = FadeIn(source, maxVolume);
+            StartCoroutine(stageAudioFadeIn);
+        }
+    }
+
+    private void FadeOutAudio(BGMType type)
+    {
+        var source = type == BGMType.MENU ? menuAudioSource : stageAudioSource;
+        var fadeInCoroutine = type == BGMType.MENU ? menuAudioFadeIn : stageAudioFadeIn;
+
+        if (fadeInCoroutine != null) StopCoroutine(fadeInCoroutine);
+        if (type == BGMType.MENU)
+        {
+            menuAudioFadeOut = FadeOut(source);
+            StartCoroutine(menuAudioFadeOut);
+        }
+        else
+        {
+            stageAudioFadeOut = FadeOut(source);
+            StartCoroutine(stageAudioFadeOut);
         }
     }
 
     private void Awake()
     {
         var another = FindObjectsOfType<MenuBGM>();
-        if (another.Length != 1)
+        if (another.Length > 1)
         {
             Destroy(gameObject);
         }
@@ -27,13 +102,15 @@ public class MenuBGM : MonoBehaviour
         {
             DontDestroyOnLoad(gameObject);
             SceneManager.sceneLoaded += OnSceneLoad;
-        }
-    }
 
-    private void Start()
-    {
-        audioSource = GetComponent<AudioSource>();
-        StartCoroutine(FadeIn());
+            menuAudioSource = gameObject.AddComponent<AudioSource>();
+            menuAudioSource.outputAudioMixerGroup = Resources.Load<AudioMixer>("Settings/MainMixer").FindMatchingGroups("Master")[0];
+            menuAudioSource.volume = 0f;
+
+            stageAudioSource = gameObject.AddComponent<AudioSource>();
+            stageAudioSource.outputAudioMixerGroup = Resources.Load<AudioMixer>("Settings/MainMixer").FindMatchingGroups("Master")[0];
+            stageAudioSource.volume = 0f;
+        }
     }
 
     private void OnDestroy()
@@ -41,16 +118,33 @@ public class MenuBGM : MonoBehaviour
         SceneManager.sceneLoaded -= OnSceneLoad;
     }
 
-    private IEnumerator FadeIn()
+    private IEnumerator FadeIn(AudioSource source, float maxVolume)
     {
-        audioSource.volume = 0f;
+        source.Play();
+
+        var startVolume = source.volume;
         float t = 0;
 
         while (t < 1)
         {
-            audioSource.volume = Mathf.Lerp(0, 0.7f, t);
+            source.volume = Mathf.Lerp(startVolume, maxVolume, t);
             t += 0.005f;
             yield return new WaitForFixedUpdate();
         }
+    }
+
+    private IEnumerator FadeOut(AudioSource source)
+    {
+        var startVolume = source.volume;
+        float t = 0;
+
+        while (t < 1)
+        {
+            source.volume = Mathf.Lerp(startVolume, 0, t);
+            t += 0.009f;
+            yield return new WaitForFixedUpdate();
+        }
+
+        source.Stop();
     }
 }
